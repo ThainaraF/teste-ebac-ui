f748f4eb1acd9b4d0a4ddb3a120d7564230247cd
Thainara Frigo <thainara.frigo@gmail.com>
2024-01-11 00:54:50 -0300

    meu primeiro commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,31 @@
+context ('funcionalidade login' , () =>{
+
+it('deve fazer login com sucesso' , ()=> {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.get('#username') . type('aluno_ebac@teste.com')
+    cy.get('#password') . type('teste@teste.com')
+    cy.get('.woocommerce-form > .button') . click()
+
+    cy.get('.page-title') . should ('contain','minha conta')
+
+
+})
+
+it.only('deve exibir uma mensagem de erro ao inserir usuario invalido' , () => {
+ cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.get('#username') . type('aluno4ebac@teste.com')
+    cy.get('#password') . type('teste@teste')
+    cy.get('.woocommerce-form > .button') . click()
+
+})
+
+it.only('deve exibir uma mensagem de erro ao inserir senha invalidos' , () => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+       cy.get('#username') . type('aluno4ebac@teste.com')
+       cy.get('#password') . type('teste@teste')
+       cy.get('.woocommerce-form > .button') . click()
+       
+   
+   })
+})
+
